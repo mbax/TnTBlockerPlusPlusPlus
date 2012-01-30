b78cec2b3fd96e9f58c595a5567243e0dbc01cd6
mbaxter <github@phozop.net>
2012-01-30 12:12:24 -0500

    Best plugin ever

---
+++ src/org/kitteh/tnt/blocker/plus/plus/Plus.java
@@ -0,0 +1,35 @@
+package org.kitteh.tnt.blocker.plus.plus;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerPreLoginEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plus extends JavaPlugin {
+
+    @Override
+    public void onDisable() {
+        this.getLogger().info("v" + this.getDescription().getVersion() + " disabled.");
+    }
+
+    @Override
+    public void onEnable() {
+        this.getLogger().info("v" + this.getDescription().getVersion() + " enabled.");
+    }
+
+    @EventHandler
+    public void prelogin(PlayerPreLoginEvent event) {
+        if (event.getName().equalsIgnoreCase("tntbass")) {
+            event.disallow(PlayerPreLoginEvent.Result.KICK_BANNED, "TNT is not allowed on this server");
+        }
+    }
+
+    // Even protects offline mode servers!
+    @EventHandler
+    public void login(PlayerLoginEvent event) {
+        if (event.getPlayer().getName().equalsIgnoreCase("tntbass")) {
+            event.disallow(PlayerLoginEvent.Result.KICK_BANNED, "TNT is not allowed on this server");
+        }
+    }
+
+}
